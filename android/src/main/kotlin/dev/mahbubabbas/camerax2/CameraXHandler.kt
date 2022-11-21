6f046751b878bff5b7d14dc812649bab63621b81
Gesya Gayatree Solih <58933926+dragongesa@users.noreply.github.com>
2022-11-21 11:55:40 +0700

    [FEAT] add classification

--- android/src/main/kotlin/dev/mahbubabbas/camerax2/CameraXHandler.kt
+++ android/src/main/kotlin/dev/mahbubabbas/camerax2/CameraXHandler.kt
@@ -182,6 +182,7 @@ class CameraXHandler(private val activity: Activity, private val textureRegistry
                         )
 
                         val realTimeOpts = FaceDetectorOptions.Builder()
+                            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
                             .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
                             .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
                             .setMinFaceSize(0.5f)
