499a2a659a3ea35907fafdec3ae327918b7a3484
Gesya Gayatree Solih <58933926+dragongesa@users.noreply.github.com>
2022-11-21 15:53:21 +0700

    [FIX] camerax2 swift

--- ios/Classes/CameraXPlugin.m
+++ ios/Classes/CameraXPlugin.m
@@ -1,11 +1,11 @@
 #import "CameraXPlugin.h"
 #if __has_include(<camerax/camerax-Swift.h>)
-#import <camerax/camerax-Swift.h>
+#import <camerax2/camerax2-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "camerax-Swift.h"
+#import "camerax2-Swift.h"
 #endif
 
 @implementation CameraXPlugin
